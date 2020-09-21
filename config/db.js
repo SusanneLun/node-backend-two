471f35d3a004da4f7331c39ed8ad73026c811240
SusanneLun <susanne.r.lundkvist@gmail.com>
2020-09-21 11:09:15 +0100

    Connected MongoDB

---
+++ config/db.js
@@ -0,0 +1,17 @@
+
+
+module.exports = {  url : mongodb+srv:
+  user_one:UA6xMG7RKatRmy6@cluster0.bdfs0.mongodb.net/Cluster0?retryWrites=true&w=majority
+};
+
+
+
+//
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://user_one:<password>@cluster0.bdfs0.mongodb.net/<dbname>?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
